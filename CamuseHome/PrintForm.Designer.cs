22786e44e5424f1e08532c3dc626846043342ef5
LiaoJunCong <liaojuncong@foxmail.com>
2016-01-03 17:56:02 +0800

    FastReport

---
+++ CamuseHome/PrintForm.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace CamuseHome
+{
+    partial class PrintForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btnPrint = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(185, 20);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // btnPrint
+            // 
+            this.btnPrint.Location = new System.Drawing.Point(203, 12);
+            this.btnPrint.Name = "btnPrint";
+            this.btnPrint.Size = new System.Drawing.Size(75, 23);
+            this.btnPrint.TabIndex = 1;
+            this.btnPrint.Text = "button1";
+            this.btnPrint.UseVisualStyleBackColor = true;
+            this.btnPrint.Click += new System.EventHandler(this.btnPrint_Click);
+            // 
+            // PrintForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(290, 129);
+            this.Controls.Add(this.btnPrint);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "PrintForm";
+            this.Text = "PrintForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btnPrint;
+    }
+}
---
+++ CamuseHome/PrintForm.cs
@@ -0,0 +1,53 @@
+﻿using FastReport;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CamuseHome
+{
+    public partial class PrintForm : Form
+    {
+        public PrintForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPrint_Click(object sender, EventArgs e)
+        {
+            Report report = new Report();
+            var reportPath = Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "Reports", "10幅横向简单画册.frx");
+            report.Load(reportPath);
+            report.SetParameterValue("Company_CName", "11");
+            report.SetParameterValue("Company_EName", "22");
+            report.SetParameterValue("Company_CAddress", "33");
+            report.SetParameterValue("Company_EAddress", "44");
+            report.SetParameterValue("Company_Tel", "55");
+            report.SetParameterValue("Company_Fax", "66");
+            report.SetParameterValue("Company_Email", "77");
+            report.SetParameterValue("UserNo", "88");
+            report.SetParameterValue("UserName", "99");
+
+            var FArray = new List<dynamic>();
+            FArray.Add(new { Prd_Pic1 = "", Prd_CName = "Prd_CName", Prd_GG = "Prd_GG", Prd_Note = "Prd_Note", Prd_ItemNo = "Prd_ItemNo" });
+            FArray.Add(new { Prd_Pic1 = "", Prd_CName = "Prd_CName", Prd_GG = "Prd_GG", Prd_Note = "Prd_Note", Prd_ItemNo = "Prd_ItemNo" });
+            FArray.Add(new { Prd_Pic1 = "", Prd_CName = "Prd_CName", Prd_GG = "Prd_GG", Prd_Note = "Prd_Note", Prd_ItemNo = "Prd_ItemNo" });
+            FArray.Add(new { Prd_Pic1 = "", Prd_CName = "Prd_CName", Prd_GG = "Prd_GG", Prd_Note = "Prd_Note", Prd_ItemNo = "Prd_ItemNo" });
+            FArray.Add(new { Prd_Pic1 = "", Prd_CName = "Prd_CName", Prd_GG = "Prd_GG", Prd_Note = "Prd_Note", Prd_ItemNo = "Prd_ItemNo" });
+            FArray.Add(new { Prd_Pic1 = "", Prd_CName = "Prd_CName", Prd_GG = "Prd_GG", Prd_Note = "Prd_Note", Prd_ItemNo = "Prd_ItemNo" });
+            FArray.Add(new { Prd_Pic1 = "", Prd_CName = "Prd_CName", Prd_GG = "Prd_GG", Prd_Note = "Prd_Note", Prd_ItemNo = "Prd_ItemNo" });
+            FArray.Add(new { Prd_Pic1 = "", Prd_CName = "Prd_CName", Prd_GG = "Prd_GG", Prd_Note = "Prd_Note", Prd_ItemNo = "Prd_ItemNo" });
+            FArray.Add(new { Prd_Pic1 = "", Prd_CName = "Prd_CName", Prd_GG = "Prd_GG", Prd_Note = "Prd_Note", Prd_ItemNo = "Prd_ItemNo" });
+            FArray.Add(new { Prd_Pic1 = "", Prd_CName = "Prd_CName", Prd_GG = "Prd_GG", Prd_Note = "Prd_Note", Prd_ItemNo = "Prd_ItemNo" });
+            report.RegisterData(FArray, "DstRpt");
+
+            report.Show();
+        }
+    }
+}
